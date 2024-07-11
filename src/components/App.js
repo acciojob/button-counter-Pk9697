c8ce9749caaedd60cd9d8b9a2127e993e97ee497
Prashant Kumar <prashant.aman9@gmail.com>
2024-07-11 17:04:48 +0530

    Complete project

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react'
+import './../styles/App.css'
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+	const [counter, setCounter] = React.useState(0)
+
+	const incrementCounter = () => {
+		setCounter((prev)=>prev+1)
+	}
+
+	return (
+		<div>
+			{/* Do not remove the main div */}
+			<p>Button clicked {counter} times</p>
+			<button onClick={incrementCounter}>Click me</button>
+		</div>
+	)
 }
 
 export default App
